368a00776d7e54054eba6e093c537000d9bc91d0
rpalsaxena <Rahul Saxena>
2020-04-10 22:53:03 +0530

    abstract data of mohfw using coronastat.py scraper

--- alibaba/spiders/coronastats.py
+++ alibaba/spiders/coronastats.py
@@ -8,9 +8,13 @@ class CoronastatsSpider(scrapy.Spider):
     start_urls = ['http://mohfw.gov.in/']
 
     def parse(self, response):
+        active = int(response.xpath('//*[@id="site-dashboard"]//ul/li[1]/strong/text()').extract()[0])
+        cured = int(response.xpath('//*[@id="site-dashboard"]//ul/li[2]/strong/text()').extract()[0])
+        deaths = int(response.xpath('//*[@id="site-dashboard"]//ul/li[3]/strong/text()').extract()[0])
+        mi = int(response.xpath('//*[@id="site-dashboard"]//ul/li[4]/strong/text()').extract()[0])
         yield{
-            'Active COVID 2019 cases':  response.xpath('//*[@id="state-data"]//table/tbody/tr[32]/td[2]/strong/text()').extract()[0],
-            'Cured / discharged cases': response.xpath('//*[@id="site-dashboard"]//ul/li[2]/strong/text()').extract()[0],
-             "Death cases": response.xpath('//*[@id="site-dashboard"]//ul/li[3]/strong/text()').extract()[0]
+            'Active COVID 2019 cases': active+cured+deaths+mi ,
+            'Cured / discharged cases': cured,
+             "Death cases": deaths,
 
         }
